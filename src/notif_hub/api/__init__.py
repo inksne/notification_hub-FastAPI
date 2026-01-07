ccf93bf4405ebde472136c0bbf165c35e0e8d948
inksne <vitalijjapr@gmail.com>
2026-01-07 13:31:47 +0300

    __init__.py

--- src/notif_hub/api/__init__.py
+++ src/notif_hub/api/__init__.py
@@ -1,7 +1,9 @@
 __all__ = [
+    'telegram_router',
     'handle_telegram_notify',
     'telegram_forbidden_error', 'telegram_retry_after_error', 'internal_server_error'
 ]
 
+from .telegram_handler import router as telegram_router
 from .telegram_handler import handle_telegram_notify
 from .exceptions import telegram_forbidden_error, telegram_retry_after_error, internal_server_error
