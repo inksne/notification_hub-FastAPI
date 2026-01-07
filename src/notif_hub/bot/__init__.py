eb452f2f6c76b58b4e5a2b54c557a96908ee791c
inksne <vitalijjapr@gmail.com>
2026-01-07 13:33:59 +0300

    __init__.py

--- src/notif_hub/bot/__init__.py
+++ src/notif_hub/bot/__init__.py
@@ -1,10 +1,10 @@
 __all__ = [
     'bot', 'dp',
     'commands_router', 'handle_start', 'handle_help',
-    'generate_start_text', 'generate_help_text', 'internal_error_text'
+    'generate_start_text', 'generate_help_text', 'generate_notify_text', 'internal_error_text'
 ]
 
 from .bot import bot, dp
 from .router import router as commands_router
 from .router import handle_start, handle_help
-from .texts import generate_start_text, generate_help_text, internal_error_text
+from .texts import generate_start_text, generate_help_text, generate_notify_text, internal_error_text
