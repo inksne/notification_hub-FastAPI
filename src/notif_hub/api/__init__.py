03388e77646675a904fb746a559f4ea8adae0985
inksne <vitalijjapr@gmail.com>
2026-01-08 13:23:03 +0300

    добавление: добавлены роутер и ручка из channels_handler.py

--- src/notif_hub/api/__init__.py
+++ src/notif_hub/api/__init__.py
@@ -1,9 +1,11 @@
 __all__ = [
-    'telegram_router',
-    'handle_telegram_notify',
+    'channels_router', 'telegram_router',
+    'handle_channels', 'handle_telegram_notify',
     'telegram_forbidden_error', 'telegram_retry_after_error', 'internal_server_error'
 ]
 
+from .channels_handler import router as channels_router
+from .channels_handler import handle_channels
 from .telegram_handler import router as telegram_router
 from .telegram_handler import handle_telegram_notify
 from .exceptions import telegram_forbidden_error, telegram_retry_after_error, internal_server_error
