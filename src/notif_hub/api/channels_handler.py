509a2a78a2239a014264aba2897a8bc73aa76d8b
inksne <vitalijjapr@gmail.com>
2026-01-08 13:21:53 +0300

    добавление: добавлена ручка для обработки рассылки сообщения по каналам

---
+++ src/notif_hub/api/channels_handler.py
@@ -0,0 +1,74 @@
+from fastapi import APIRouter
+from starlette import status
+
+import logging
+
+from .exceptions import internal_server_error
+from .telegram_handler import handle_telegram_notify
+from ..config import configure_logging
+from ..basemodels import ChannelsHandlerModel, TelegramHandlerModel
+
+
+
+router = APIRouter(tags=['Channels Handler'], prefix='/api/v1')
+
+
+configure_logging()
+logger = logging.getLogger(__name__)
+
+
+
+'''
+Входящий json:
+{
+  "messages": {
+    "telegram": '...',
+    "email": '...',
+  },
+  "channels": ["telegram", "email"],
+  "targets": {
+    'telegram': '123456789',
+    'email': 'email@example.com'
+  }
+}
+'''
+
+
+
+@router.post('/channels', status_code=status.HTTP_204_NO_CONTENT)
+async def handle_channels(data: ChannelsHandlerModel) -> None:
+    try:
+        # try:
+        #     async with httpx.AsyncClient() as client:
+        #         if 'telegram' in data.targets:
+        #             request_data = TelegramHandlerModel(
+        #                 message=data.messages['telegram'],
+        #                 username=data.targets['telegram'],
+        #             )
+
+        #             await client.post(
+        #                 url=f'{APP_HOST}/api/v1/telegram',
+        #                 json=request_data.model_dump(),
+        #             )
+
+        # except (httpx.HTTPStatusError, httpx.RequestError) as e:
+        #     logger.error(e)
+        #     raise internal_server_error
+
+
+        if 'telegram' in data.targets:
+            telegram_username = data.targets['telegram']
+
+            if telegram_username.startswith('@'):
+                telegram_username = telegram_username[1:]
+
+            request_data = TelegramHandlerModel(
+                message=data.messages['telegram'],
+                username=telegram_username
+            )
+
+            await handle_telegram_notify(request_data)
+
+    except Exception as e:
+        logger.error(e)
+        raise internal_server_error
