c91b48e720eda551e82235f456660784fb525aeb
inksne <vitalijjapr@gmail.com>
2026-01-08 13:26:50 +0300

    добавление: добавлена Pydantic BaseModel ChannelsHandlerModel

--- src/notif_hub/basemodels.py
+++ src/notif_hub/basemodels.py
@@ -5,3 +5,9 @@ from pydantic import BaseModel
 class TelegramHandlerModel(BaseModel):
     message: str
     username: str
+
+
+class ChannelsHandlerModel(BaseModel):
+    messages: dict[str, str]
+    channels: list[str]
+    targets: dict[str, str]
