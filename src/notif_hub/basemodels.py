e5175c63b84514d7658fd12df8e8bd24813439fb
inksne <vitalijjapr@gmail.com>
2026-01-09 11:51:37 +0300

    добавление: добавлена новая модель: EmailRequestModel"

--- src/notif_hub/basemodels.py
+++ src/notif_hub/basemodels.py
@@ -11,3 +11,12 @@ class ChannelsHandlerModel(BaseModel):
     messages: dict[str, str]
     channels: list[str]
     targets: dict[str, str]
+
+
+class EmailRequestModel(BaseModel):
+    subject: str
+    body: str
+    to_email: str
+    host: str
+    port: int
+    use_starttls: bool 
