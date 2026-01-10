6b82c9f1d8fe7fdb0e60ae1d7d0fa4d1c943fde6
inksne <vitalijjapr@gmail.com>
2026-01-10 13:37:56 +0300

    __init__.py

--- src/notif_hub/api/__init__.py
+++ src/notif_hub/api/__init__.py
@@ -1,6 +1,7 @@
 __all__ = [
-    'channels_router', 'telegram_router',
-    'handle_channels', 'handle_telegram_notify',
+    'channels_router', 'telegram_router', 'email_router',
+    'handle_channels', 'handle_telegram_notify', 'handle_email_notify',
+    'send_email_via_smtp', 'build_email_message',
     'telegram_forbidden_error', 'telegram_retry_after_error', 'internal_server_error'
 ]
 
@@ -8,4 +9,7 @@ from .channels_handler import router as channels_router
 from .channels_handler import handle_channels
 from .telegram_handler import router as telegram_router
 from .telegram_handler import handle_telegram_notify
+from .email_handler import router as email_router
+from .email_handler import handle_email_notify
+from .email_helpers import send_email_via_smtp, build_email_message
 from .exceptions import telegram_forbidden_error, telegram_retry_after_error, internal_server_error
