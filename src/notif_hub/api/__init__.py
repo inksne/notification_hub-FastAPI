1444d6f9da2dd888dee8c43368426b7214648a9a
inksne <vitalijjapr@gmail.com>
2026-01-11 12:29:39 +0300

    __init__.py

--- src/notif_hub/api/__init__.py
+++ src/notif_hub/api/__init__.py
@@ -1,8 +1,11 @@
 __all__ = [
-    'channels_router', 'telegram_router', 'email_router',
-    'handle_channels', 'handle_telegram_notify', 'handle_email_notify',
+    'channels_router', 'telegram_router', 'email_router', 'webhook_router',
+    'handle_channels', 'handle_telegram_notify', 'handle_email_notify', 'handle_webhook_notify',
     'send_email_via_smtp', 'build_email_message',
-    'telegram_forbidden_error', 'telegram_retry_after_error', 'internal_server_error'
+    'telegram_forbidden_error', 'telegram_retry_after_error',
+    'webhook_bad_request_error', 'webhook_unauthorized_error', 'webhook_forbidden_error', 'webhook_method_not_allowed_error',
+    'webhook_not_found_error', 'webhook_too_many_requests_error', 'webhook_unavailable_for_legal_reasons_error',
+    'internal_server_error'
 ]
 
 from .channels_handler import router as channels_router
@@ -11,5 +14,18 @@ from .telegram_handler import router as telegram_router
 from .telegram_handler import handle_telegram_notify
 from .email_handler import router as email_router
 from .email_handler import handle_email_notify
+from .webhook_handler import router as webhook_router
+from .webhook_handler import handle_webhook_notify
 from .email_helpers import send_email_via_smtp, build_email_message
-from .exceptions import telegram_forbidden_error, telegram_retry_after_error, internal_server_error
+from .exceptions import (
+    telegram_forbidden_error,
+    telegram_retry_after_error,
+    webhook_bad_request_error,
+    webhook_unauthorized_error,
+    webhook_forbidden_error,
+    webhook_not_found_error,
+    webhook_method_not_allowed_error,
+    webhook_too_many_requests_error,
+    webhook_unavailable_for_legal_reasons_error,
+    internal_server_error
+)
