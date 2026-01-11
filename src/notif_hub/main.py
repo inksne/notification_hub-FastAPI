7279ae50a2d2376c1e67ae8b60f44b0eb6d28aab
inksne <vitalijjapr@gmail.com>
2026-01-11 12:31:55 +0300

    добавление: добавлено подключение webhook_router

--- src/notif_hub/main.py
+++ src/notif_hub/main.py
@@ -9,7 +9,7 @@ import uvicorn
 from .database.database import create_db_and_tables
 from .templates import templates_router
 from .bot import bot, dp, commands_router
-from .api import channels_router, telegram_router, email_router
+from .api import channels_router, telegram_router, email_router, webhook_router
 
 
 @asynccontextmanager
@@ -33,6 +33,7 @@ app.include_router(templates_router)
 app.include_router(channels_router)
 app.include_router(telegram_router)
 app.include_router(email_router)
+app.include_router(webhook_router)
 
 
 
