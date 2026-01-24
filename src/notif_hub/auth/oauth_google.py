4afa6c590c2704f638e4fffbc9929261fc4a6947
inksne <vitalijjapr@gmail.com>
2026-01-24 11:28:30 +0300

    добавление: добавлен state в query_params

--- src/notif_hub/auth/oauth_google.py
+++ src/notif_hub/auth/oauth_google.py
@@ -1,15 +1,21 @@
 import urllib.parse
+import secrets
 
 from ..config import constant_settings
 
 
 
 def generate_google_oauth_redirect_uri():
+    state = secrets.token_urlsafe(16)
+
+    #TODO: добавить сохранение state в бд.
+
     query_params = {
         "redirect_uri": constant_settings.REDIRECT_URI,
         "client_id": constant_settings.OAUTH_GOOGLE_CLIENT_ID,
         "response_type": "code",
-        "scope": " ".join(["openid", "profile", "email"])
+        "scope": " ".join(["openid", "profile", "email"]),
+        "state": state
     }
 
     query_string = urllib.parse.urlencode(query=query_params, quote_via=urllib.parse.quote)
