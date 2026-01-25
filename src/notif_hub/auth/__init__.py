6122b9e85788fc59322c982cbff14368b52d2e93
inksne <vitalijjapr@gmail.com>
2026-01-25 12:43:46 +0300

    __init__.py

--- src/notif_hub/auth/__init__.py
+++ src/notif_hub/auth/__init__.py
@@ -1,13 +1,17 @@
 __all__ = [
-    'auth_router',
+    'google_auth_router',
     'get_google_oauth_redirect_uri', 'handle_google_code',
+    'github_auth_router',
+    'get_github_oauth_redirect_uri', 'handle_github_code',
     '_jwk_client', '_get_jwk_client', 'parse_user_data',
     'generate_google_oauth_redirect_uri',
     'invalid_response_error', 'invalid_token_error', 'internal_server_error'
 ]
 
-from .router import router as auth_router
-from .router import get_google_oauth_redirect_uri, handle_google_code
+from .google_router import router as google_auth_router
+from .google_router import get_google_oauth_redirect_uri, handle_google_code
+from .github_router import router as github_auth_router
+from .github_router import get_github_oauth_redirect_uri, handle_github_code
 from .jwt_parse import _jwk_client, _get_jwk_client, parse_user_data
 from .oauth_google import generate_google_oauth_redirect_uri
 from .exceptions import invalid_response_error, invalid_token_error, internal_server_error
