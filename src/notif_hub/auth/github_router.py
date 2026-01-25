05e33633e484421eb87b1664d742f22f604adb78
inksne <vitalijjapr@gmail.com>
2026-01-25 12:41:28 +0300

    добавление: добавлена авторизация с помощью github через oauth

---
+++ src/notif_hub/auth/github_router.py
@@ -0,0 +1,90 @@
+from typing import Annotated
+from fastapi import APIRouter, Body
+from fastapi.responses import RedirectResponse
+from starlette import status
+
+import logging
+import httpx
+
+from ..config import configure_logging, constant_settings
+from ..database.managers import redis_manager
+from .oauth_google import generate_github_oauth_redirect_uri
+from .exceptions import (
+    internal_server_error,
+    invalid_response_error,
+    invalid_token_error,
+    state_not_found_error
+)
+
+
+
+configure_logging()
+logger = logging.getLogger(__name__)
+
+
+router = APIRouter(tags=["Auth"], prefix='/github')
+
+
+
+@router.get("/github/url")
+def get_github_oauth_redirect_uri():
+    uri = generate_github_oauth_redirect_uri()
+
+    return RedirectResponse(url=uri, status_code=status.HTTP_302_FOUND)
+
+
+
+
+@router.post("/github/callback")
+async def handle_github_code(code: Annotated[str, Body()], state: Annotated[str, Body()]):
+    if not await redis_manager.get_state(state):
+        raise state_not_found_error
+
+    async with httpx.AsyncClient() as client:
+        try:
+            headers = {"Accept": "application/json"}
+
+            response = await client.post(
+                url=constant_settings.GITHUB_TOKEN_URL,
+                data={
+                    "client_id": constant_settings.OAUTH_GITHUB_CLIENT_ID,
+                    "client_secret": constant_settings.OAUTH_GITHUB_CLIENT_SECRET,
+                    "redirect_uri": constant_settings.GITHUB_REDIRECT_URI,
+                    "code": code
+                },
+                headers=headers
+            )
+            response.raise_for_status()
+
+            try:
+                data = response.json()
+
+            except ValueError as e:
+                logger.error(e)
+                raise invalid_response_error
+
+            access_token = data.get("access_token")
+
+            if not access_token:
+                raise invalid_response_error
+
+            user_response = await client.get(
+                url=constant_settings.GITHUB_ACCESS_URL,
+                headers={
+                    "Accept": "application/vnd.github+json",
+                    "Authorization": f"Bearer {access_token}",
+                    "X-GitHub-Api-Version": "2022-11-28",
+                },
+            )
+            user_response.raise_for_status()
+
+            try:
+                return {"user": user_response.json()}
+
+            except ValueError as e:
+                logger.error(e)
+                raise invalid_response_error
+
+        except httpx.HTTPStatusError as e:
+            logger.error(e)
+            raise internal_server_error
---
+++ src/notif_hub/auth/oauth_github.py
@@ -0,0 +1,24 @@
+import urllib.parse
+import secrets
+
+from ..config import constant_settings
+from ..database.managers import redis_manager
+
+
+
+async def generate_github_oauth_redirect_uri():
+    state = secrets.token_urlsafe(16)
+
+    await redis_manager.add_state(state=state)
+
+    query_params = {
+        "redirect_uri": constant_settings.GITHUB_REDIRECT_URI,
+        "client_id": constant_settings.OAUTH_GITHUB_CLIENT_ID,
+        "scope": "user",
+        "state": state
+    }
+
+    query_string = urllib.parse.urlencode(query=query_params, quote_via=urllib.parse.quote)
+    base_url = constant_settings.GITHUB_AUTH_URL
+
+    return f"{base_url}?{query_string}"
