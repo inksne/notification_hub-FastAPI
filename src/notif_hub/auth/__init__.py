3ff394f7db641ee445c3acd1b8a57758f38365e6
inksne <vitalijjapr@gmail.com>
2026-01-26 18:21:25 +0300

    __init__.py

--- src/notif_hub/auth/__init__.py
+++ src/notif_hub/auth/__init__.py
@@ -5,7 +5,18 @@ __all__ = [
     'get_github_oauth_redirect_uri', 'handle_github_code',
     '_jwk_client', '_get_jwk_client', 'parse_user_data',
     'generate_google_oauth_redirect_uri',
-    'invalid_response_error', 'invalid_token_error', 'internal_server_error', 'state_not_found_error'
+    'invalid_response_error', 'invalid_token_error', 'internal_server_error', 'state_not_found_error',
+    'bad_email_error', 'bad_token_error', 'not_found_access_token_error', 'not_found_refresh_token_error',
+    'not_found_token_user_error', 'unauthed_error', 'conflict_name_error', 'user_exists_error',
+    'cookie_auth_router',
+    'create_jwt', 'create_access_token', 'create_refresh_token',
+    'UserSchema',
+    'encode_jwt', 'decode_jwt', 'hash_password', 'validate_password',
+    'get_current_access_token_payload', 'get_current_refresh_token_payload',
+    'validate_token_type', 'get_current_auth_user_from_access_token_of_type',
+    'get_current_auth_user_from_refresh_token_of_type', 'get_current_auth_user',
+    'get_current_auth_user_for_refresh', 'get_user_by_token_sub',
+    'validate_auth_user_db', 'validate_email'
 ]
 
 from .google_router import router as google_auth_router
@@ -14,4 +25,33 @@ from .github_router import router as github_auth_router
 from .github_router import get_github_oauth_redirect_uri, handle_github_code
 from .jwt_parse import _jwk_client, _get_jwk_client, parse_user_data
 from .oauth_google import generate_google_oauth_redirect_uri
-from .exceptions import invalid_response_error, invalid_token_error, internal_server_error, state_not_found_error
+from .exceptions import (
+    invalid_response_error,
+    invalid_token_error,
+    internal_server_error, 
+    state_not_found_error,
+    bad_email_error,
+    bad_token_error,
+    not_found_access_token_error,
+    not_found_refresh_token_error,
+    not_found_token_user_error,
+    unauthed_error,
+    conflict_name_error,
+    user_exists_error
+)
+from .cookie_auth.auth import router as cookie_auth_router
+from .cookie_auth.helpers import create_jwt, create_access_token, create_refresh_token
+from .cookie_auth.schemas import UserSchema
+from .cookie_auth.utils import encode_jwt, decode_jwt, hash_password, validate_password
+from .cookie_auth.validation import (
+    get_current_access_token_payload,
+    get_current_refresh_token_payload,
+    validate_token_type,
+    get_current_auth_user_from_access_token_of_type,
+    get_current_auth_user_from_refresh_token_of_type,
+    get_current_auth_user,
+    get_current_auth_user_for_refresh,
+    get_user_by_token_sub,
+    validate_auth_user_db,
+    validate_email
+)
