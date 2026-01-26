859bcca4854622a9e462bf9ce88d0fa763f69625
inksne <vitalijjapr@gmail.com>
2026-01-26 18:24:04 +0300

    добавление: добавлены новые константы, связанные с аутентификацией

--- src/notif_hub/config.py
+++ src/notif_hub/config.py
@@ -4,6 +4,8 @@ import os
 import logging
 from typing import Final
 
+from .basemodels import AuthJWT
+
 
 
 class ConstantSettings(BaseSettings):
@@ -25,8 +27,8 @@ class ConstantSettings(BaseSettings):
     GOOGLE_TOKEN_URL: Final[str] = "https://oauth2.googleapis.com/token"
     GOOGLE_AUTH_URL: Final[str] = "https://accounts.google.com/o/oauth2/v2/auth"
 
-    GOOGLE_JWKS_URI = "https://www.googleapis.com/oauth2/v3/certs"
-    GOOGLE_ISSUERS = ("https://accounts.google.com", "accounts.google.com")
+    GOOGLE_JWKS_URI: Final[str] = "https://www.googleapis.com/oauth2/v3/certs"
+    GOOGLE_ISSUERS: tuple = ("https://accounts.google.com", "accounts.google.com")
 
     OAUTH_GITHUB_CLIENT_ID: Final[str]
     OAUTH_GITHUB_CLIENT_SECRET: Final[str]
@@ -41,6 +43,10 @@ class ConstantSettings(BaseSettings):
     EMAIL_HOST: Final[str] = "smtp.yandex.ru"
     EMAIL_PORT: Final[int] = 465
 
+    TOKEN_TYPE_FIELD: Final[str] = "type"
+    ACCESS_TOKEN_TYPE: Final[str] = "access"
+    REFRESH_TOKEN_TYPE: Final[str] = "refresh"
+
     model_config = SettingsConfigDict(
         env_file=os.path.join(os.path.dirname(__file__), '..', '..', '.env'),
         env_file_encoding='utf-8'
@@ -53,6 +59,13 @@ class DBSettings(BaseSettings):
     db_url: str = f'postgresql+asyncpg://{constant_settings.POSTGRES_USER}:{constant_settings.POSTGRES_PASSWORD}@{constant_settings.POSTGRES_HOST}:5432/{constant_settings.POSTGRES_DB}'
 
 db_settings = DBSettings()
+
+
+class AuthSettings(BaseSettings):
+    auth_jwt: AuthJWT = AuthJWT()
+
+
+auth_settings = AuthSettings()
 
 
 def configure_logging(level: int = logging.INFO) -> None:
