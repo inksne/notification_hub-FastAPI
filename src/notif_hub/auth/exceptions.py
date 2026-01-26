1a494545b09e83f23d0adcbd737983f757661404
inksne <vitalijjapr@gmail.com>
2026-01-26 18:21:08 +0300

    добавление: добавлены новые обработки ошибок

--- src/notif_hub/auth/exceptions.py
+++ src/notif_hub/auth/exceptions.py
@@ -25,3 +25,51 @@ state_not_found_error = HTTPException(
     status.HTTP_401_UNAUTHORIZED,
     detail='Время, отведённое на авторизацию, истекло. Попробуйте снова.'
 )
+
+
+bad_email_error = HTTPException(
+    status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+    detail='Email некорректен. Введите действительный адрес электронной почты.'
+)
+
+
+bad_token_error = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED, 
+    detail=f'Неверный токен.',
+)
+
+
+not_found_access_token_error = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Access токен не найден в куки.",
+)
+
+
+not_found_refresh_token_error = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Refresh токен не найден в куки.",
+)
+
+
+not_found_token_user_error = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail='Неверный токен.',
+)
+
+
+unauthed_error = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Неверный логин или пароль."
+)
+
+
+conflict_name_error = HTTPException(
+    status_code=status.HTTP_409_CONFLICT,
+    detail='Данное имя пользователя уже используется. Попробуйте другое.'
+)
+
+
+user_exists_error = HTTPException(
+    status_code=status.HTTP_409_CONFLICT,
+    detail='Пользователь с такими данными уже существует.'
+)
