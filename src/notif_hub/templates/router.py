7068e90da97a2fb335611cd083191fa0a1a35641
inksne <vitalijjapr@gmail.com>
2026-01-27 18:33:26 +0300

    добавление: добавлены новые шаблоны, связанные с регистрацией и с аутентификацией

--- src/notif_hub/templates/router.py
+++ src/notif_hub/templates/router.py
@@ -25,3 +25,18 @@ templates = Jinja2Templates(directory=str(Path(__file__).resolve().parent))
 @router.get("/", response_class=HTMLResponse)
 async def get_base_page(request: Request) -> _TemplateResponse:
     return templates.TemplateResponse(request, "base.html")
+
+
+@router.get("/about", response_class=HTMLResponse)
+async def get_about_page(request: Request) -> _TemplateResponse:
+    return templates.TemplateResponse(request, "about_us.html")
+
+
+@router.get("/register", response_class=HTMLResponse)
+async def get_register_page(request: Request) -> _TemplateResponse:
+    return templates.TemplateResponse(request, "register.html")
+
+
+@router.get("/authenticated", response_class=HTMLResponse)
+async def get_authenticated_page(request: Request) -> _TemplateResponse:
+    return templates.TemplateResponse(request, "authenticated.html")
