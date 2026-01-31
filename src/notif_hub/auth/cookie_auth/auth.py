912b03898eb082bdacd14b527f797ec4f78ec337
inksne <vitalijjapr@gmail.com>
2026-01-31 11:55:09 +0300

    добавление: теперь куки не удаляются при закрытии браузера, из-за чего токены всегда живут полную жизнь

--- src/notif_hub/auth/cookie_auth/auth.py
+++ src/notif_hub/auth/cookie_auth/auth.py
@@ -1,14 +1,14 @@
 from fastapi import APIRouter, Depends, Response, Form, HTTPException
-from fastapi.responses import JSONResponse, RedirectResponse
+from fastapi.responses import RedirectResponse
 from fastapi.security import HTTPBearer
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
 
 import logging
-from datetime import timedelta
+from datetime import datetime, timedelta, timezone
 
 from .helpers import create_access_token, create_refresh_token
-from .validation import get_current_auth_user_for_refresh, validate_auth_user_db, validate_email
+from .validation import get_current_auth_user, get_current_auth_user_for_refresh, validate_auth_user_db, validate_email
 from .utils import hash_password
 from .schemas import UserSchema
 from ..exceptions import conflict_name_error, bad_email_error, user_exists_error, internal_server_error
@@ -66,12 +66,16 @@ async def auth_user_issue_jwt(response: Response, user: UserSchema = Depends(val
     access_token = create_access_token(user)
     refresh_token = create_refresh_token(user)
 
+    access_max_age = int(timedelta(minutes=auth_settings.auth_jwt.access_token_expire_minutes).total_seconds())
+    refresh_max_age = int(timedelta(days=auth_settings.auth_jwt.refresh_token_expire_days).total_seconds())
+
     response.set_cookie(
         key="access_token",
         value=access_token,
         httponly=False,
         secure=False,
-        max_age=int(timedelta(minutes=auth_settings.auth_jwt.access_token_expire_minutes).total_seconds())
+        max_age=access_max_age,
+        expires=datetime.now(timezone.utc) + timedelta(seconds=access_max_age)
     )
 
     response.set_cookie(
@@ -79,32 +83,57 @@ async def auth_user_issue_jwt(response: Response, user: UserSchema = Depends(val
         value=refresh_token,
         httponly=False,
         secure=False,
-        max_age=int(timedelta(days=auth_settings.auth_jwt.refresh_token_expire_days).total_seconds())
+        max_age=refresh_max_age,
+        expires=datetime.now(timezone.utc) + timedelta(seconds=refresh_max_age)
     )
 
     return TokenInfo(access_token=access_token, refresh_token=refresh_token)
 
 
 @router.post("/refresh")
-async def refresh_jwt(current_user: UserSchema = Depends(get_current_auth_user_for_refresh)) -> JSONResponse:
+async def refresh_jwt(
+    response: Response,
+    current_user: UserSchema = Depends(get_current_auth_user_for_refresh)
+) -> TokenInfo:
     new_access_token = create_access_token(current_user)
-    response = JSONResponse(content={"access_token": new_access_token})
+    new_refresh_token = create_refresh_token(current_user)
+
+    access_max_age = int(timedelta(minutes=auth_settings.auth_jwt.access_token_expire_minutes).total_seconds())
+    refresh_max_age = int(timedelta(days=auth_settings.auth_jwt.refresh_token_expire_days).total_seconds())
 
     response.set_cookie(
         key="access_token",
         value=new_access_token,
         httponly=False,
         secure=False,
-        max_age=int(timedelta(minutes=auth_settings.auth_jwt.access_token_expire_minutes).total_seconds())
+        max_age=access_max_age,
+        expires=datetime.now(timezone.utc) + timedelta(seconds=access_max_age)
     )
 
-    return response
+    response.set_cookie(
+        key="refresh_token",
+        value=new_refresh_token,
+        httponly=False,
+        secure=False,
+        max_age=refresh_max_age,
+        expires=datetime.now(timezone.utc) + timedelta(seconds=refresh_max_age)
+    )
+
+    return TokenInfo(access_token=new_access_token, refresh_token=new_refresh_token)
 
 
 @router.post('/logout')
 async def logout(response: Response) -> RedirectResponse:
     response = RedirectResponse('/', status_code=status.HTTP_303_SEE_OTHER)
+
     response.delete_cookie(key="access_token", httponly=False, secure=False)
     response.delete_cookie(key="refresh_token", httponly=False, secure=False)
 
     return response
+
+
+@router.get('/check_tokens')
+async def check_token(
+    current_user: User = Depends(get_current_auth_user)
+) -> RedirectResponse:
+    return RedirectResponse('/authenticated', status.HTTP_303_SEE_OTHER)
