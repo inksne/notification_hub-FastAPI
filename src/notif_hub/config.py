a89352f30b56896cc2bbe12f8350c9d46938a92c
inksne <vitalijjapr@gmail.com>
2026-01-31 11:59:34 +0300

    обновление: изменена версия: 0.1.14 -> 0.1.15

--- src/notif_hub/config.py
+++ src/notif_hub/config.py
@@ -47,7 +47,7 @@ class ConstantSettings(BaseSettings):
     ACCESS_TOKEN_TYPE: Final[str] = "access"
     REFRESH_TOKEN_TYPE: Final[str] = "refresh"
 
-    APP_VERSION: Final[str] = "0.1.14"
+    APP_VERSION: Final[str] = "0.1.15"
 
     model_config = SettingsConfigDict(
         env_file=os.path.join(os.path.dirname(__file__), '..', '..', '.env'),
