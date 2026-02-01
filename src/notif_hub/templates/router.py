da0c4650c1535cb7c5ba79c1368ff818782744d4
inksne <vitalijjapr@gmail.com>
2026-02-01 10:19:44 +0300

    добавление: добавлены эндпоинты для взаимодействия с уведомлениям

--- src/notif_hub/templates/router.py
+++ src/notif_hub/templates/router.py
@@ -1,13 +1,17 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from starlette.templating import _TemplateResponse
+from sqlalchemy.ext.asyncio import AsyncSession
 
 import logging
 from pathlib import Path
 
 from ..config import auth_settings, configure_logging
-
+from ..auth import get_current_auth_user
+from ..database.database import get_async_session
+from ..database.managers import psql_manager
+from ..database.models import User, Notification
 
 configure_logging()
 log = logging.getLogger(__name__)
@@ -41,11 +45,49 @@ async def get_login_page(request: Request) -> _TemplateResponse:
 
 
 @router.get("/authenticated", response_class=HTMLResponse)
-async def get_authenticated_page(
-    request: Request,
-) -> _TemplateResponse:
+async def get_authenticated_page(request: Request) -> _TemplateResponse:
     return templates.TemplateResponse(
         request, "authenticated.html", {
             "ACCESS_TOKEN_LIFETIME": auth_settings.auth_jwt.access_token_expire_minutes * 60000 # ms
         }
     )
+
+
+@router.get("/authenticated/notifications", response_class=HTMLResponse)
+async def get_notifications_page(
+    request: Request, current_user: User = Depends(get_current_auth_user)
+) -> _TemplateResponse:
+    return templates.TemplateResponse(
+        request, "notifications.html", {
+            "ACCESS_TOKEN_LIFETIME": auth_settings.auth_jwt.access_token_expire_minutes * 60000 # ms
+        }
+    )
+
+
+@router.get("/authenticated/notifications/get_all_notifications", response_model=None)
+async def get_all_notifications(
+    current_user: User = Depends(get_current_auth_user),
+    session: AsyncSession = Depends(get_async_session)
+) -> list[Notification]:
+    notifications = await psql_manager.get_notifications(user_id=current_user.id, session=session)
+
+    return notifications
+
+
+@router.post("/authenticated/notifications/add_notification")
+async def add_notification(
+    channels: list[str],
+    content: str,
+    current_user: User = Depends(get_current_auth_user),
+    session: AsyncSession = Depends(get_async_session)
+) -> None:
+    await psql_manager.add_notification(channels=channels, content=content, user_id=current_user.id, session=session)
+
+
+@router.delete("/authenticated/notifications/delete_notification")
+async def delete_notification(
+    notification_id: int,
+    current_user: User = Depends(get_current_auth_user),
+    session: AsyncSession = Depends(get_async_session)
+) -> None:
+    await psql_manager.delete_notification(notification_id=notification_id, user_id=current_user.id, session=session)
