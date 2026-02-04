c71f33a29e2bdbdec938c45e4bce56c2272a52d8
inksne <vitalijjapr@gmail.com>
2026-02-04 20:00:43 +0300

    добавление: добавлена новая модель: Refresh_tokens

--- src/notif_hub/database/models.py
+++ src/notif_hub/database/models.py
@@ -1,5 +1,7 @@
-from sqlalchemy import ForeignKey, String, Text, Integer, ARRAY
+from sqlalchemy import ForeignKey, String, Text, Integer, ARRAY, func, DateTime, Boolean
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
+from datetime import datetime
 
 
 class Base(DeclarativeBase):
@@ -15,6 +17,7 @@ class User(Base):
     password: Mapped[str] = mapped_column(Text, nullable=False)
 
     notifications: Mapped['Notification'] = relationship('Notification', back_populates='user')
+    refresh_tokens: Mapped['Refresh_token'] = relationship('Refresh_token', back_populates='user')
 
 
 class Notification(Base):
@@ -27,6 +30,19 @@ class Notification(Base):
     recipient: Mapped[list[str]] = mapped_column(ARRAY(String), nullable=False)
 
     user: Mapped['User'] = relationship('User', back_populates='notifications')
+
+
+class Refresh_token(Base):
+    __tablename__ = 'refresh_tokens'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'), nullable=False)
+    refresh_token_hash: Mapped[str] = mapped_column(Text, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    expires_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
+    is_revoked: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+
+    user: Mapped['User'] = relationship('User', back_populates='refresh_tokens')
 
 
 class Chat_id(Base):
